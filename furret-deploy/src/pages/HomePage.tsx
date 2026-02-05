fdcd0dc0cb0cccae43f7cce9921461a6f51a443e
guleakbar <97419521+guleakbar@users.noreply.github.com>
2026-02-05 16:16:51 +0500

    Create HomePage component for routing

---
+++ furret-deploy/src/pages/HomePage.tsx
@@ -0,0 +1,21 @@
+import Hero from '../sections/Hero';
+import Features from '../sections/Features';
+import Deals from '../sections/Deals';
+import Analytics from '../sections/Analytics';
+import Testimonials from '../sections/Testimonials';
+import CTA from '../sections/CTA';
+
+const HomePage = () => {
+  return (
+    <>
+      <Hero />
+      <Features />
+      <Deals />
+      <Analytics />
+      <Testimonials />
+      <CTA />
+    </>
+  );
+};
+
+export default HomePage;
